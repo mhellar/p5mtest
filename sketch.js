3837b424c03fce237ce875b681aa8bbeaf4af353
mhellar <mark.hellar@gmail.com>
2020-03-29 11:17:42 -0700

    fix

--- sketch.js
+++ sketch.js
@@ -4,6 +4,12 @@ function setup() {
     stroke(0);
 }
 
+function draw() {
+    textSize(32);
+    fill(0, 102, 153);
+    text('Touch To Draw', 10, 30);
+}
+
 function touchMoved() {
     line(mouseX, mouseY, pmouseX, pmouseY);
     return false;
